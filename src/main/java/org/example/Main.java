9065eea161bc86f6aa8c209d4a9accd3bf4836ca
vitinh0z <victor10.vg23@gmail.com>
2025-10-01 00:33:58 -0300

    e main recebendo dados e mostrando a url no terminal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,25 @@
 package org.example;
 
+
+import br.com.victor.automacao.model.URLModel;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Titulo da vaga: ");
+        String titulo = scanner.nextLine();
+
+        System.out.println("Localização da Vaga: ");
+        String localizacao = scanner.nextLine();
+
+        URLModel url = new URLModel(titulo,localizacao);
+
+        System.out.println(url.getUrl());
+
+    }
 }
