835b7f41e84b4770c0cf2dd4cbffe94c0e6cd9aa
vitinh0z <victor10.vg23@gmail.com>
2025-10-01 00:30:09 -0300

    fiz URLEncoder que recebe o titulo e local, e monta a url para o site da Indeed

---
+++ src/main/java/br/com/victor/automacao/model/URLModel.java
@@ -0,0 +1,27 @@
+package br.com.victor.automacao.model;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class URLModel extends Pesquisa{
+
+    public URLModel(String titulo, String localizacao) {
+        super(titulo, localizacao);
+    }
+
+    public String getUrl (){
+        try {
+            String cargo = URLEncoder.encode(getTitulo(), StandardCharsets.UTF_8.toString());
+            String local = URLEncoder.encode(getLocalizacao(), StandardCharsets.UTF_8.toString());
+
+            return String.format("https://br.indeed.com/jobs?q=%s&l=%s", cargo, local);
+
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Erro ao codificar a url");
+        }
+
+
+
+    }
+}
